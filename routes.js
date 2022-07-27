b14683f500bbde0e72a86826f612c0ca43d325f4
tibo127 <till.barnstedt@gamil.com>
2022-07-27 21:17:31 +0200

    Foundation Login System

--- routes.js
+++ routes.js
@@ -14,6 +14,31 @@ const addRoutes = function(i18nextMiddleware, i18n, supportedLanguages, app, hos
     //     }
     // });
 
+    i18nextMiddleware.addRoute(i18n, '/login', supportedLanguages, app, 'get', function(req, res) { //route.products/route.harddrives/route.overview
+        console.log(req.i18n.languages);
+        res.render("login_page", {title: req.i18n.t("test-title"), lang: req.i18n.language});
+    });
+
+    i18nextMiddleware.addRoute(i18n, '/registration', supportedLanguages, app, 'get', function(req, res) { //route.products/route.harddrives/route.overview
+        console.log(req.i18n.languages);
+        res.render("registration", {title: req.i18n.t("test-title"), lang: req.i18n.language});
+    });
+
+    i18nextMiddleware.addRoute(i18n, '/changepassword', supportedLanguages, app, 'get', function(req, res) { //route.products/route.harddrives/route.overview
+        console.log(req.i18n.languages);
+        res.render("change_password", {title: req.i18n.t("test-title"), lang: req.i18n.language});
+    });
+
+    i18nextMiddleware.addRoute(i18n, '/changeemail', supportedLanguages, app, 'get', function(req, res) { //route.products/route.harddrives/route.overview
+        console.log(req.i18n.languages);
+        res.render("change-email", {title: req.i18n.t("test-title"), lang: req.i18n.language});
+    });
+
+    i18nextMiddleware.addRoute(i18n, '/changeusername', supportedLanguages, app, 'get', function(req, res) { //route.products/route.harddrives/route.overview
+        console.log(req.i18n.languages);
+        res.render("change-username", {title: req.i18n.t("test-title"), lang: req.i18n.language});
+    });
+
     i18nextMiddleware.addRoute(i18n, '/', supportedLanguages, app, 'get', function(req, res) { //route.products/route.harddrives/route.overview
         console.log(req.i18n.language);
         // if (req.i18n.language === 'en') {
