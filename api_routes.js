bbbfa9cf7a5e62822c338ced5ba175699c9e150e
Nick1st <54022594+Nick1st@users.noreply.github.com>
2022-10-06 20:08:56 +0200

    WS-9 Login System (#14)
    * ws-9 Build the basics of the login system. If a user is logged in `req.session.isLoggedIn` is `true`, else it's `false` or undefined. (req being the request parameter from a router function).
    
    * The Knex Store is strange and does still not work correctly
    
    * Add a logout button

--- api_routes.js
+++ api_routes.js
@@ -1,5 +1,8 @@
 import express from "express";
 import {supportedLanguages} from "./app.js";
+import {getPwd} from "./mysql-connector.js";
+import bcrypt from 'bcryptjs';
+import {requestLogin} from "./routes.js";
 
 const router = express.Router();
 
@@ -21,5 +24,39 @@ router.get('/lang-available', function (req, res) {
     }
     return res.status(200).json(Object.fromEntries(map1));
 })
+
+router.post('/login', function (req, res) {
+    let email = "";
+    if (req.body.email == null || req.body.password == null) {
+
+    } else {
+        email = req.body.email.toLowerCase().trim();
+        console.log(email);
+    }
+    getPwd(email).then(hash => {
+        console.log(hash);
+        bcrypt.compare(req.body.password, hash, function(err, match) {
+            if (match) { // Auth successful
+                req.session.failed = false;
+                req.session.isLoggedIn = true;
+                return res.redirect(303, req.body.from);
+            } else { // Auth failed
+                req.session.failed = true;
+                req.session.isLoggedIn = false;
+                req.session.from = req.body.from;
+                return res.redirect(303, "/login");
+            }
+        });
+
+    });
+});
+
+router.get('/logout', function (req, res) {
+    if (req.session.isLoggedIn !== true) {
+        requestLogin(res, req);
+    }
+    req.session.destroy();
+    return res.redirect(303, "/");
+});
 
 export { router };
--- routes.js
+++ routes.js
@@ -1,37 +1,55 @@
-const addRoutes = function(i18nextMiddleware, i18n, supportedLanguages, app, hostname, portSave) { //TODO Fix this mess
+const addRoutes = function(i18nextMiddleware, i18n, supportedLanguages, app) { //TODO Fix this mess
     i18nextMiddleware.addRoute(i18n, '/:lng/path2/', supportedLanguages, app, 'get', function(req, res) { //route.products/route.harddrives/route.overview
         console.log(req.i18n.language);
-        res.render("index", {title: req.i18n.t("test-title"), lang: req.i18n.language, site: "XYZ", nonce: res.locals.cspNonce});
+        res.render("index", addDefaultConfig(req, res, {title: req.i18n.t("test-title"), site: "XYZ"}));
     });
 
     i18nextMiddleware.addRoute(i18n, '/login', supportedLanguages, app, 'get', function(req, res) {
         console.log(req.i18n.languages);
-        res.render("login", {title: getTitle(req, "login"), lang: req.i18n.language, site: "login", nonce: res.locals.cspNonce});
+        requestLogin(res, req)
     });
 
     i18nextMiddleware.addRoute(i18n, '/registration', supportedLanguages, app, 'get', function(req, res) {
         console.log(req.i18n.languages);
-        res.render("registration", {title: getTitle(req, "registration"), lang: req.i18n.language, site: "registration", nonce: res.locals.cspNonce});
+        res.render("registration", addDefaultConfig(req, res, {title: getTitle(req, "registration"), site: "registration"}));
     });
 
     i18nextMiddleware.addRoute(i18n, '/forgot', supportedLanguages, app, 'get', function(req, res) {
         console.log(req.i18n.languages);
-        res.render("forgot", {title: getTitle(req, "forgot"), lang: req.i18n.language, site: "forgot", nonce: res.locals.cspNonce});
+        res.render("forgot", addDefaultConfig(req, res, {title: getTitle(req, "forgot"), site: "forgot"}));
     });
 
     i18nextMiddleware.addRoute(i18n, '/', supportedLanguages, app, 'get', function(req, res) {
         console.log(req.i18n.language);
-        // if (req.i18n.language === 'en') {
-        //     res.writeHead(303, {'Location': `https://${hostname}:${portSave}` + '/en/'}); // 303 -> Get other
-        //     res.end();
-        // } else {
-        res.render("index", {title: getTitle(req, "test-title"), lang: req.i18n.language, site: "index", nonce: res.locals.cspNonce});
-        // }
+        res.render("index", addDefaultConfig(req, res, {title: getTitle(req, "test-title"), site: "index"}));
     });
 }
 
 function getTitle(req, key) {
     return req.i18n.t(key) + req.i18n.t("title-common");
 }
+
+function addDefaultConfig(req, res, obj) {
+    return {
+        ...obj,
+        ...res.options,
+        lang: req.i18n.language,
+        _csrfToken: req.csrfToken(),
+        isLoggedIn: req.session.isLoggedIn
+    };
+}
+
+const requestLogin = function (res, req) {
+    if (req.session.from == null || typeof req.session.from !== 'string') {
+        req.session.from = "/"
+    }
+    let fail = "";
+    if (req.session.failed === true) {
+        fail = 'Login failed: Wrong credentials';
+    }
+    res.render("login", addDefaultConfig(req, res, {title: getTitle(req, "login"), site: "login", from: req.session.from, failed: fail}));
+}
+
+export { requestLogin }
 
 export { addRoutes }
